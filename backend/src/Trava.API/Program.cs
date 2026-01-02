438e007194183ef39e39bf631dc5630d56b638e7
Minh Thuận <ndmthuanwork97@gmail.com>
2026-01-02 18:52:19 +0700

    ui swagger for product

--- backend/src/Trava.API/Program.cs
+++ backend/src/Trava.API/Program.cs
@@ -63,12 +63,8 @@ builder.Services.Configure<JwtSettings>(
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 await app.MigrationsDatabaseAsync();
 
