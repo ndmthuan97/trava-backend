26cfa698bbd56089c3aa058109cf2c110e4bb3e9
Minh Thuận <ndmthuanwork97@gmail.com>
2026-01-23 13:49:15 +0700

    Change: route user profile

--- backend/src/Trava.API/Controllers/UserController.cs
+++ backend/src/Trava.API/Controllers/UserController.cs
@@ -1,11 +1,7 @@
-using System;
 using System.Security.Claims;
-using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Trava.API.Models;
 using Trava.Application.Features.Users.Commands;
 using Trava.Application.Features.Users.Queries;
@@ -43,7 +39,7 @@ namespace Trava.API.Controllers
             });
         }
 
-        [HttpGet("{id:guid}/profile")]
+        [HttpGet("profile/{id:guid}")]
         [Authorize(Roles = $"{nameof(Role.SystemAdmin)},{nameof(Role.User)}")]
         [ProducesResponseType(typeof(ApiResponse<UserResponse>), StatusCodes.Status200OK)]
         public async Task<IActionResult> GetProfileById([FromRoute] Guid id)
