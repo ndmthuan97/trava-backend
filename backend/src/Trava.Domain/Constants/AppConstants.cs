1eff15209cc07506d1f41164b8bfbf4cf15e5cd2
Minh Thuận <ndmthuanwork97@gmail.com>
2026-03-02 09:19:50 +0700

    update: default avatar for user

--- backend/src/Trava.Domain/Constants/AppConstants.cs
+++ backend/src/Trava.Domain/Constants/AppConstants.cs
@@ -7,7 +7,7 @@ namespace Trava.Domain.Constants
 {
     public static class AppConstants
     {
-        public const string DEFAULT_AVATAR = "https://fwogtpqkinfjeanusbfw.supabase.co/storage/v1/object/sign/avatars/jack-meme_-1.jpg?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV8zMDI1NjNlYy03ZTYyLTRlMDAtODU4Mi1jOGRhZDFkYzQ0NzAiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJhdmF0YXJzL2phY2stbWVtZV8tMS5qcGciLCJpYXQiOjE3Njk3NDE3OTQsImV4cCI6MTgwMTI3Nzc5NH0.nnSYuTK9i6unYnlK3ciQXfDV2xHNrBJlsCiaSj031Co";
+        public const string DEFAULT_AVATAR = "https://fwogtpqkinfjeanusbfw.supabase.co/storage/v1/object/public/avatars/default-avatar.png";
         public const int MAX_PAGE_SIZE = 70;
         public const int DEFAULT_PAGE_SIZE = 12;
         public const int DEFAULT_PAGE_INDEX = 1;
