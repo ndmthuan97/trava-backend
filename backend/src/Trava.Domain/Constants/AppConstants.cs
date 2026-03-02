8356d586ae3a6776936a9091cce0e4eb2af7d246
Minh Thuận <ndmthuanwork97@gmail.com>
2026-03-02 23:40:57 +0700

    update: page size

--- backend/src/Trava.Domain/Constants/AppConstants.cs
+++ backend/src/Trava.Domain/Constants/AppConstants.cs
@@ -9,7 +9,7 @@ namespace Trava.Domain.Constants
     {
         public const string DEFAULT_AVATAR = "https://fwogtpqkinfjeanusbfw.supabase.co/storage/v1/object/public/avatars/default-avatar.png";
         public const int MAX_PAGE_SIZE = 70;
-        public const int DEFAULT_PAGE_SIZE = 12;
+        public const int DEFAULT_PAGE_SIZE = 8;
         public const int DEFAULT_PAGE_INDEX = 1;
     }
 }
